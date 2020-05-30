a9a4149aa98de4069290cec236b4c77f432aa5bd
pat-s <patrick.schratz@gmail.com>
2020-05-30 12:49:11 +0200

    first try

---
+++ tic.R
@@ -0,0 +1,12 @@
+get_stage("install") %>%
+  add_step(step_install_github("dataseries")) %>%
+  add_step(step_install_github("timetk")) %>%
+  add_step(step_install_github("flexdashboard")) %>%
+
+get_stage("before_deploy") %>%
+  add_step(step_setup_ssh()) %>%
+  add_step(step_setup_push_deploy(""))
+
+get_stage("deploy") %>%
+  add_code_step(rmarkdown::render("docs/index.Rmd")) %>%
+  add_step(step_do_push_deploy(commit_paths = "docs/index.html"))
